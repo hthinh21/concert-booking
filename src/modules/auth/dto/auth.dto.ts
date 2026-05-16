d086701040ba1d95245d17a87ba1d092ab0be540
hthinh21 <dothinh795@gmail.com>
2026-05-16 19:42:52 +0700

    add auth dto

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class LoginDto {
+    @ApiProperty({ example: 'customer@test.com' })
+    @IsEmail()
+    email: string;
+
+    @ApiProperty({ example: 'password123' })
+    @IsString()
+    @MinLength(6)
+    password: string;
+}
+
+export class RegisterDto {
+    @ApiProperty({ example: 'Thinh Do' })
+    @IsString()
+    name: string;
+
+    @ApiProperty({ example: 'customer@test.com' })
+    @IsEmail()
+    email: string;
+
+    @ApiProperty({ example: 'password123' })
+    @IsString()
+    @MinLength(6)
+    password: string;
+}
