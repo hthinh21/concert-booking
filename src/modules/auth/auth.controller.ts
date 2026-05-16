b2c0a186d40635bee6e1cd3c451b8e2ac7dae2e1
hthinh21 <dothinh795@gmail.com>
2026-05-16 19:43:57 +0700

    add auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,34 +1,22 @@
-// import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-// import { AuthService } from './auth.service';
-// import { CreateAuthDto } from './dto/create-auth.dto';
-// import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from './dto/auth.dto';
 
-// @Controller('auth')
-// export class AuthController {
-//   constructor(private readonly authService: AuthService) {}
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) { }
 
-//   @Post()
-//   create(@Body() createAuthDto: CreateAuthDto) {
-//     return this.authService.create(createAuthDto);
-//   }
+  @Post('login')
+  @ApiOperation({ summary: 'Login and get JWT token' })
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
 
-//   @Get()
-//   findAll() {
-//     return this.authService.findAll();
-//   }
-
-//   @Get(':id')
-//   findOne(@Param('id') id: string) {
-//     return this.authService.findOne(+id);
-//   }
-
-//   @Patch(':id')
-//   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-//     return this.authService.update(+id, updateAuthDto);
-//   }
-
-//   @Delete(':id')
-//   remove(@Param('id') id: string) {
-//     return this.authService.remove(+id);
-//   }
-// }
+  @Post('register')
+  @ApiOperation({ summary: 'Register new customer account' })
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
+}
